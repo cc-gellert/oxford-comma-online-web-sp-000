837f58d5774d595375ac4b38cafab24151865e75
Chloe Gell <gellert.chloe@gmail.com>
2020-01-21 21:10:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
   newStr = "" 
+  if(array.length == 1)
+    return "#{array}"
+  end 
   array.each_with_index do |ele, idx| 
     if(idx != -1)
       newStr += "#{ele}, "
