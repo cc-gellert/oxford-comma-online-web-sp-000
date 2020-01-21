b0a5dc733fec7b06d3a4c83834f214ed0adf2aa6
Chloe Gell <gellert.chloe@gmail.com>
2020-01-21 21:06:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   newStr = "" 
   array.each_with_idx do |ele, idx| 
-  if(idx == 0)
-    newStr += "#{ele}, "
-  elsif (idx != -1)
-    newStr += "#{ele}, "
-  else 
-    newStr += "and #{ele}" "  
-  end 
+    if(idx == 0)
+      newStr += "#{ele}, "
+    elsif(idx != -1)
+      newStr += "#{ele}, "
+    else 
+      newStr += "and #{ele}" "  
+    end 
   return newStr 
 end
